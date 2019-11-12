60c1848b15c3fa234bf5a608c0781923a538a1eb
tama <tama@ttk1.net>
2019-11-12 23:41:43 +0900

    Jetty全然わからん。

--- src/main/java/net/ttk1/jetty_example/Main.java
+++ src/main/java/net/ttk1/jetty_example/Main.java
@@ -1,9 +1,15 @@
 package net.ttk1.jetty_example;
 
-import java.util.Arrays;
+import net.ttk1.jetty_example.servlet.HogeServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(3000);
+        ServletHandler handler = new ServletHandler();
+        handler.addServletWithMapping(HogeServlet.class, "/");
+        server.setHandler(handler);
+        server.start();
     }
 }

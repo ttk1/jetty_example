ba570b667c7377a8e4ba01fbc5ac75b1fa2d7f6e
tama <tama@ttk1.net>
2019-11-12 23:44:41 +0900

    微修正

--- src/main/java/net/ttk1/jetty_example/servlet/HogeServlet.java
+++ src/main/java/net/ttk1/jetty_example/servlet/HogeServlet.java
@@ -1,6 +1,5 @@
 package net.ttk1.jetty_example.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,7 +7,7 @@ import java.io.IOException;
 
 public class HogeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.getWriter().println("Hello, World");
     }
 }
